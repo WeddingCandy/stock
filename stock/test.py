0533c67e25db52732cc66b0f8584ebc4a1422814
Chans <chenyouwenxyz@hotmail.com>
2017-10-30 23:21:03 +0800

    This the vision in home practice.

--- stock/test.py
+++ stock/test.py
@@ -16,27 +16,21 @@ import pymysql
 loginResult = c.start()
 #设置下载数据的区间
 tradedate_list = c.tradedates("2017-10-20", "2017-10-26").Dates
-list = ['1']
 #tradedate_list = ["20071231", "20081231", "20091231", "20101231", "20111231", "20121231", "20131231", "20141231", "20151231","20161231"]
 #这是得到当前的A股代码-----test B股,total num = 100
 secID_list = c.sector("2000033013","2017-06-30").Codes #,"20170630"
 #下载数据
-tables =pd.DataFrame(index=secID_list, columns=list)
-for i in range(0,3):
-    table = pd.DataFrame(index=secID_list, columns=tradedate_list)  # use pandas.DataFrame
-    for dt in tradedate_list:
-        data2 = c.css(secID_list, "PE,PEG,PB,EV", "ReportDate=" + dt, "PredictYear=2017")
-        for stk in data2.Codes:#for-cycle to write data into table
-            table.loc[stk, dt] = data2.Data[stk][i]  # The index of dataframe
-    tables = pd.concat([tables,table],axis=1,join="inner")
-
-tables.index.names = ["secID"]  # para name
-
-
+table = pd.DataFrame(index=secID_list, columns=tradedate_list)#use pandas.DataFrame
+for dt in tradedate_list:
+    data1 = c.css(secID_list, "TOTALSHARE", "ReportDate="+dt)#merge?????
+    data2 = c.css(secID_list, "PE,PEG,PB,EV", "ReportDate="+ dt,"PredictYear=2017")
+    for stk in data2.Codes:  #for-cycle to write data into table
+        table.loc[stk,dt] = data2.Data[stk][1]#The index of dataframe
+table.index.names = ["secID"]# para name
 logoutResult = c.stop()
 
 #保存为csv文件到桌面
-table.to_csv('test6.csv')
+table.to_csv('test5.csv')
 #保存到mysql数据库
 engine= create_engine("mysql+pymysql://root:112233@localhost/east",echo = False)
-table.to_sql("test6", engine, if_exists="replace", index=True, dtype={'secID':types.CHAR(12)})
+table.to_sql("test5", engine, if_exists="replace", index=True, dtype={'secID':types.CHAR(12)})
